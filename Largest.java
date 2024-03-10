a28e7043c3a22097413fd389868c8994ef211087
Abhishek Shinde <01fe22bca012@kletech.ac.in>
2024-03-10 11:49:04 +0530

    Largest of 3 nos

---
+++ Largest.java
@@ -0,0 +1,21 @@
+class Largest{
+    public static void main(String args[]){
+    int num1=10;
+        int num2=28;
+        int num3=39;
+
+        if((num1>num2) && (num1>num3)){
+            System.out.println("Num1 is the largest!!!");
+        }
+        else if((num2>num1) && (num2>num3)){
+            System.out.println("Num2 is the largest!!!");
+
+        }
+        else{
+            System.out.println("Num3 is the largest!!!");
+
+        }
+
+    }
+    
+}
